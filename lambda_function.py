1bfad05fb065a5fc60a8064e56e349c137421aa8
Mohammad Rahman <mcmogaming@gmail.com>
2023-05-09 17:19:30 -0400

    test

--- lambda_function.py
+++ lambda_function.py
@@ -5,29 +5,24 @@ def lambda_handler(event, context):
     
     # location = event['CodePipeline.job'].data.inputArtifacts[0]
 
-    layerzip = boto3.resource('s3').get_object(Bucket='lambdalayersbuildbucket',Key='messageLibrary.zip')
+    layerzip = boto3.client('s3').get_object(Bucket='lambdalayersbuildbucket',Key='messageLibrary.zip')
     
-    # boto3.client.publish_layer_version(
-    # LayerName='messageLibraryLayer',
-    # Description='message library',
-    # Content={
-    #     'S3Bucket': 'lambdalayersbuildbucket',
-    #     'S3Key': 'messageLibrary.zip',
-    #     'S3ObjectVersion': 'string',
-    #     'ZipFile': b'bytes'
-    # },
-    # CompatibleRuntimes=[
-    #     'nodejs'|'nodejs4.3'|'nodejs6.10'|'nodejs8.10'|'nodejs10.x'|'nodejs12.x'|'nodejs14.x'|'nodejs16.x'|'java8'|'java8.al2'|'java11'|'python2.7'|'python3.6'|'python3.7'|'python3.8'|'python3.9'|'dotnetcore1.0'|'dotnetcore2.0'|'dotnetcore2.1'|'dotnetcore3.1'|'dotnet6'|'nodejs4.3-edge'|'go1.x'|'ruby2.5'|'ruby2.7'|'provided'|'provided.al2'|'nodejs18.x'|'python3.10'|'java17',
-    # ],
-    # LicenseInfo='string',
-    # CompatibleArchitectures=[
-    #     'x86_64'|'arm64',
-    # ]
-    #)
+    layer = boto3.client('lambda').publish_layer_version(
+    LayerName='messageLibraryLayer',
+    Description='message library',
+    Content=layerzip,
+    CompatibleRuntimes=[
+        'python3.9'
+    ],
+    LicenseInfo='string',
+    CompatibleArchitectures=[
+        'x86_64',
+    ]
+    )
     
     
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!'),
-        'layerzip': layerzip
+        'layer': layer
     }
